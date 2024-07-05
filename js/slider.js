1fdcffc47174cdfddb737f377c6639df2b54004b
Charlieriveraa <charlierivera541@gmail.com>
2024-07-05 15:34:28 +0800

    done no slider

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+var slider = tns({
+    container: ".homeBanner__slider",
+    items: 1,
+    speed: 800,
+    gutter: 100,
+    slideBy: "page",
+    autoplay: true,
+    navPosition: "bottom",
+    axis: "vertical",
+    // containerControls: "#controls",
+    // prevButton: ".prev",
+    // nextButton: ".next",
+  });
